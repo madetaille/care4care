ca5e2e89517f760f5d973c350bfa371fbf1b002e
madetaille <mattieu.detaille@gmail.com>
2014-11-24 17:51:29 +0100

    commit view accept job

--- c4c_app/views.py
+++ c4c_app/views.py
@@ -5,38 +5,46 @@ from django.views import generic
 
 from c4c_app.models import *
 
-def createJob(request, c4cuser_id):
-    maker = get_object_or_404(C4CUser, pk = c4cuser_id)
-    try:
-        user = C4CUser.objects.get(pk=request.POST['user'])    
-    except (KeyError, C4CUser.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'create_job.html', {
-            'error_message': "You enter an invalide user.",
-        })
+def createJob(request):
+    maker = get_object_or_404(C4CUser, user=request.user)
+    job = None
+    if(request.POST['kind'] == 'offer'):
+        job = C4CJob(created_by = maker, done_by = maker, title = request.POST['title'],
+                     description = request.POST['description'], location = request.POST['location'],
+                     duration = request.POST['duration'], start_date = request.POST['start'],
+                     end_date = request.POST['end'])
     else:
-        job = None
-        if(request.POST['kind'] == 'offer'):
-            job = C4CJob(created_by = maker, done_by = user, title = request.POST['title'],
-                         description = request.POST['description'], location = request.POST['location'],
-                         duration = request.POST['duration'], start_date = request.POST['start'],
-                         end_date = request.POST['end'])
-        else:
-            job = C4CJob(created_by = maker, asked_by = user, title = request.POST['title'],
-                         description = request.POST['description'], location = request.POST['location'],
-                         duration = request.POST['duration'], start_date = request.POST['start'],
-                         end_date = request.POST['end'])
-        job.save()
+        job = C4CJob(created_by = maker, done_by = maker, title = request.POST['title'],
+                     description = request.POST['description'], location = request.POST['location'],
+                     duration = request.POST['duration'], start_date = request.POST['start'],
+                     end_date = request.POST['end'])
+    job.save()
         
-        return HttpResponseRedirect(reverse('c4c:job_detail', args=(job.id,)))
+    return HttpResponseRedirect(reverse('c4c:job_detail', args=(job.id,)))
 
 class JobDetail(generic.DetailView):
     model = C4CJob
     template_name = 'job_detail.html'
     
 def acceptJob(request, c4cjob_id):
-    return
-    
+    job = get_object_or_404(C4CJob, pk = c4cjob_id)
+    user_job = job.created_by
+    user_site = get_object_or_404(C4CUser, user = request.user)
+    if user_site == user_job:
+        return render(request, 'job_detail.html', {
+            'c4cjob' : job,
+            'error_message': "You can't accept a demand/offer that you created !.",
+        })
+    else:
+        if job.done_by == None:
+            job.done_by = user_site
+            job.save()
+            return HttpResponseRedirect(reverse('c4c:job_detail', args=(job.id)))
+        else:
+            job.asked_by = user_site
+            job.save()
+            return HttpResponseRedirect(reverse('c4c:job_detail', args=(job.id)))
+
 def confirmJob(request, c4cjob_id):
     return
     
