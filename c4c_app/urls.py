c2d40b786aee0ac0a8ec55592f75342123357cfb
Muntasib <mohammad.syed@student.uclouvain.be>
2014-11-25 16:31:11 +0100

    User related work shoud start with content of this commit

--- c4c_app/urls.py
+++ c4c_app/urls.py
@@ -4,6 +4,8 @@ from c4c_app import views
 
 urlpatterns = patterns('',
     #url(r'^$', views.home, name='home'),
+    
+    #Jobs
     url(r'^jobdetail/(?P<pk>\d+)/$', views.JobDetail.as_view(), name='job_detail'),
     url(r'^(?P<c4cjob_id>\d+)/acceptjob$', views.acceptJob, name='accept_job'),
     url(r'^(?P<c4cjob_id>\d+)/donejob$', views.doneJob, name='done_job'),
@@ -14,4 +16,7 @@ urlpatterns = patterns('',
     url(r'^userjobs/$', views.UserJobs.as_view(), name='user_jobs'),
     url(r'^createjob/$', views.createJob, name='create_job'),
     url(r'jobcreation/$',views.JobCreation.as_view(), name='job_creation'),
+    #User
+    url(r'^userdetail/(?P<pk>\d+)/$', views.UserDetail.as_view(), name='user_detail'),
+    url(r'^network/$', views.PersonalNetwork.as_view(), name='network'),
 )
--- c4c_app/views_user.py
+++ c4c_app/views_user.py
@@ -6,14 +6,14 @@ from django.utils import timezone
 
 from c4c_app.models import *
 
+class UserDetail(generic.DetailView):
+    model = C4CUser
+    template_name = 'user_detail.html'
+
 class PersonalNetwork(generic.ListView):
     template_name = 'network.html'
     context_object_name = 'network_list'
     
     def get_queryset(self):
         member = get_object_or_404(C4CUser, user=self.request.user)
-        res=[]
-        res.append(member.jobs_created.all())
-        res.append(member.jobs_asked.all())
-        res.append(member.jobs_accepted.all())
-        return res 
+        return member.network.all()
