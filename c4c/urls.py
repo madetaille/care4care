f9aa04ebc60c8543a3091be582dad2339613d476
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-11-26 11:51:43 +0100

    Put the website at the root of the web server

--- c4c/urls.py
+++ c4c/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'c4c.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^c4c/', include('c4c_app.urls',namespace='c4c')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       # Examples:
+                       # url(r'^$', 'c4c.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'', include('c4c_app.urls', namespace='c4c'))
+                       )
