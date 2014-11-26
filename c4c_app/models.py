ac9ccbbe031ae174fc27095530aa5abc784606e7
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-11-26 17:16:14 +0100

    Time account is now updated automagically (NB: pip3 install django-model-utils)

--- c4c_app/models.py
+++ c4c_app/models.py
@@ -1,5 +1,8 @@
 import django.contrib.auth.models
 from django.db import models
+from django.db.models.signals import pre_save, post_save, pre_delete
+from django.dispatch import receiver
+from model_utils import FieldTracker
 
 
 class C4CUser(models.Model):
@@ -21,6 +24,20 @@ class C4CUser(models.Model):
         """ Allows to clearly see the username in the administration """
         return str(self.user)
 
+    def recompute_time_account(self):
+        """ Recomputes the time account of a user """
+        count = 0
+        for job in self.jobs_asked.filter(complete=True):
+            count -= job.duration
+        for job in self.jobs_accepted.filter(complete=True):
+            count += job.duration
+        for donation in self.donations_made.all():
+            count -= donation.amount
+        for donation in self.donations_received.all():
+            count += donation.amount
+        self.time_account = count
+        self.save()
+
 
 class C4CJob(models.Model):
 
@@ -41,6 +58,9 @@ class C4CJob(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(default=None, null=True, blank=True)
     complete = models.BooleanField(default=False)
+
+    # Allow to track changes
+    tracker = FieldTracker(fields=['asked_by', 'done_by'])
 
     def is_offer(self):
         """ Returns true if the job is an offer """
@@ -68,6 +88,9 @@ class C4CDonation(models.Model):
     date = models.DateTimeField()
     message = models.CharField(max_length=1000)
     amount = models.IntegerField()  # in minutes
+
+    # Allow to track changes
+    tracker = FieldTracker(fields=['sender', 'receiver'])
 
     def __str__(self):
         """ Allows to clearly see the donations in the administration """
@@ -113,3 +136,53 @@ class C4CEvent(models.Model):
     def __str__(self):
         """ Allows to clearly see the events in the administration """
         return "{} - {}".format(str(self.date), self.name)
+
+
+# Signal handlers for time account update
+def _get_set_without_none(listt):
+    """ Get a set that does not include None from the list ``listt`` """
+    return set([i for i in listt if i is not None])
+
+
+def _update_user_time_account_by_id(id_set):
+    """ Update time account of users whose ids are in the set ``id_set`` """
+    for user_pk in id_set:
+        user = C4CUser.objects.get(pk=user_pk)
+        user.recompute_time_account()
+
+
+@receiver(pre_save, sender=C4CJob)
+def handle_job_pre_save(sender, instance, **kwargs):
+    """ Registers user that may be impacted by the save of the job """
+    instance.user_to_update = _get_set_without_none([instance.asked_by_id, instance.done_by_id, instance.tracker.previous('asked_by'), instance.tracker.previous('asked_by')])
+
+
+@receiver(post_save, sender=C4CJob)
+def handle_job_post_save(sender, instance, **kwargs):
+    """ Recomputes the time account of the users impacted by the save of the job """
+    _update_user_time_account_by_id(instance.user_to_update)
+
+
+@receiver(pre_delete, sender=C4CJob)
+def handle_job_delete(sender, instance, **kwargs):
+    """ Recomputes the time account of the users impacted by the save of the job """
+    user_to_update = _get_set_without_none([instance.asked_by_id, instance.done_by_id, instance.tracker.previous('asked_by'), instance.tracker.previous('asked_by')])
+    _update_user_time_account_by_id(user_to_update)
+
+
+@receiver(pre_save, sender=C4CDonation)
+def handle_donation_pre_save(sender, instance, **kwargs):
+    """ Registers user that may be impacted by the save of the donation """
+    instance.user_to_update = _get_set_without_none([instance.sender_id, instance.receiver_id, instance.tracker.previous('sender'), instance.tracker.previous('receiver')])
+
+
+@receiver(post_save, sender=C4CDonation)
+def handle_donation_post_save(sender, instance, **kwargs):
+    """ Recomputes the time account of the users impacted by the save of the donation """
+    _update_user_time_account_by_id(instance.user_to_update)
+
+
+@receiver(pre_delete, sender=C4CDonation)
+def handle_donation_delete(sender, instance, **kwargs):
+    user_to_update = _get_set_without_none([instance.sender_id, instance.receiver_id, instance.tracker.previous('sender'), instance.tracker.previous('receiver')])
+    _update_user_time_account_by_id(user_to_update)
