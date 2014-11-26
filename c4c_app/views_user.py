760e1b60c864c11e79a3f37c988019459ee98078
ROBIN <robinballarini_3@hotmail.com>
2014-11-26 16:03:10 +0100

    User profile and profile edition

--- c4c_app/views_user.py
+++ c4c_app/views_user.py
@@ -4,13 +4,31 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 from c4c_app.models import C4CUser
 
 class UserDetail(generic.DetailView):
-    model = C4CUser
-    template_name = 'user_detail.html'
+	model = C4CUser
+	template_name = 'user_detail.html'
+    
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super(UserDetail, self).get_context_data(**kwargs)
+		viewer = get_object_or_404(C4CUser, user=self.request.user)
+		context['viewer'] = viewer
+		return context
 
+class UserEdit(generic.edit.UpdateView):
+	model = C4CUser
+	fields = ['address', 'birthday']
+	template_name= 'c4cuser_edit.html'
+		
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.save()
+		return HttpResponseRedirect(reverse('c4c:user_detail', args=(self.object.pk,)))
+        
 class PersonalNetwork(generic.ListView):
     template_name = 'network.html'
     context_object_name = 'network_list'
