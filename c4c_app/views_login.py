47647572a16cc5f5c6b2101cada874153d26bec6
Thibault <thibault.vandermosten@student.uclouvain.be>
2014-12-01 11:35:32 +0100

    Fixing login and password

--- c4c_app/views_login.py
+++ c4c_app/views_login.py
@@ -32,7 +32,7 @@ def user_login(request):
                 return HttpResponse("Your account is disabled.")
         else:
             # Bad login details were provided. So we can't log the user in.
-            return HttpResponse("Invalid login details supplied.")
+            return render(request, template_name, {'invalid':True})
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
