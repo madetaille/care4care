a7f60154778f044ec4f9160615e8ebb0fcad2b6b
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-12-03 14:16:06 +0100

    Only display users in administration if they are in the branch of the current officer

--- c4c_app/admin.py
+++ c4c_app/admin.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User, Group
 
 from c4c_app.models import C4CBranch, C4CDonation, C4CEvent, C4CJob, C4CUser
 
+#########################################
+#                                       #
+# Models accessible only to super-admin #
+#                                       #
+#########################################
+
 
 class C4CAdminUser(admin.StackedInline):
 
@@ -18,14 +24,6 @@ class UserAdmin(OriginalUserAdmin):
     inlines = (C4CAdminUser, )
 
 
-class C4CAdminJob(admin.ModelAdmin):
-    pass
-
-
-class C4CAdminDonation(admin.ModelAdmin):
-    pass
-
-
 class C4CAdminBranch(admin.ModelAdmin):
     fields = ('name', 'address', 'main_user')
     readonly_fields = ('group', 'officers_group')
@@ -36,6 +34,7 @@ class C4CAdminBranch(admin.ModelAdmin):
         if not change:
             obj.group = Group.objects.create(name=("users - " + obj.name))
             obj.officers_group = Group.objects.create(name=("officers - " + obj.name))
+            obj.group.user_set.add(obj.main_user)
             obj.officers_group.user_set.add(obj.main_user)
         admin.ModelAdmin.save_model(self, request, obj, form, change)
 
@@ -52,9 +51,61 @@ class C4CAdminBranch(admin.ModelAdmin):
             o.officers_group.delete()
             o.delete()
 
+########################################
+#                                      #
+# Models accessible to non-super-admin #
+#                                      #
+########################################
+
+
+def _get_user_queryset_from_officer(user):
+    """ Return a queryset asking for all the users that the officer can moderate """
+    groups = [a.group for a in list(C4CBranch.objects.filter(officers_group__in=list(user.groups.all())))]
+    return User.objects.filter(groups__in=groups)
+
+
+class C4CAdminJob(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """ Restrict users that are displayed in the form for non-superadmin """
+        if not request.user.is_superuser:  # officer
+            if db_field.name in ("created_by", "asked_by", "done_by"):
+                kwargs["queryset"] = _get_user_queryset_from_officer(request.user)
+        return super(C4CAdminJob, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super(C4CAdminDonation, self).get_queryset(request)
+        if not request.user.is_superuser:  # officer
+            users = list(_get_user_queryset_from_officer(request.user))
+            return qs.filter(created_by__in=users, asked_by__in=users, done_by__in=users)
+        return qs
+
+
+class C4CAdminDonation(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """ Restrict users that are displayed in the form for non-superadmin """
+        if not request.user.is_superuser:  # officer
+            if db_field.name in ("sender", "receiver"):
+                kwargs["queryset"] = _get_user_queryset_from_officer(request.user)
+        return super(C4CAdminDonation, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super(C4CAdminDonation, self).get_queryset(request)
+        if not request.user.is_superuser:  # officer
+            users = list(_get_user_queryset_from_officer(request.user))
+            return qs.filter(sender__in=users, receiver__in=users)
+        return qs
+
 
 class C4CAdminEvent(admin.ModelAdmin):
-    pass
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """ Restrict users that are displayed in the form for non-superadmin """
+        if not request.user.is_superuser:  # officer
+            if db_field.name in ("user"):
+                kwargs["queryset"] = _get_user_queryset_from_officer(request.user)
+        return super(C4CAdminEvent, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.unregister(User)
