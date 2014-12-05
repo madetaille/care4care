debe41a77aa2ed8761c50d64a56d667ad5d7910c
madetaille <mattieu.detaille@gmail.com>
2014-12-05 17:36:13 +0100

    commit comments

--- c4c_app/views_aboutus.py
+++ c4c_app/views_aboutus.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def aboutus(request):
-    #return render_to_response('what_is_c4c.html')
+    """ Display the aboutus menu """
     template_name = 'about_us.html'
     if request.method == 'GET':
         return render_to_response(template_name, context_instance=RequestContext(request))
