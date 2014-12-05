c1300a4d7404bb50db57f1c87c55ea8841eb7ba8
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2014-12-05 17:05:18 +0100

    Registration/profile/edit profile done!

--- c4c_app/views_user.py
+++ c4c_app/views_user.py
@@ -1,24 +1,24 @@
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.core.urlresolvers import reverse
-from django.core.exceptions import ObjectDoesNotExist
-from django.views import generic
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.forms.fields import DateField, EmailField
-from django.contrib.admin import widgets
+import datetime
+
 from django import forms
+from django.contrib.admin import widgets
 from django.contrib.auth import authenticate, login
-
-from django.utils.translation import ugettext as _
-
-from c4c_app.models import C4CUser
-from c4c_app.views_error403 import error403
-from c4c import settings
-from django.template.loader import get_template
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMultiAlternatives
+from django.core.urlresolvers import reverse
+from django.forms.fields import DateField, EmailField, ImageField
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import Context
+from django.template.loader import get_template
+from django.utils.translation import ugettext as _
+from django.views import generic
+
+from c4c import settings
+from c4c_app.models import C4CUser, C4CBranch
+from c4c_app.views_error403 import error403
+
 
 class UserDetail(generic.DetailView):
     model = C4CUser
@@ -30,121 +30,77 @@ class UserDetail(generic.DetailView):
         viewer = get_object_or_404(C4CUser, user=self.request.user)
         context['viewer'] = viewer
 
+        context['is_in_network'] = viewer == context['c4cuser'] or context['c4cuser'].filter(pk=viewer.pk).exists()
+
         # get branches
         context['branches'] = self.object.get_branches()
         return context
 
-class UserEdit(generic.edit.UpdateView):
-    template_name = 'base_user_edit.html'
-    model = User
-    fields = ['username','first_name','last_name','email']
 
-    def post(self, request, **kwargs):
-        ret = super(UserEdit, self).post(request, **kwargs)
-        ownerpk = int(self.kwargs.get('pk'))
-        viewer = get_object_or_404(User, pk=self.request.user.pk)
-        viewerpk = viewer.pk
+class UserForm(forms.Form):
+    password = forms.CharField(label=_('Password'), max_length=10, widget=forms.PasswordInput, required=False)
+    password_confirm = forms.CharField(label=_('Password (confirmation)'), max_length=10, widget=forms.PasswordInput, required=False)
+    address = forms.CharField(max_length=300)
+    birthday = DateField(widget=widgets.AdminDateWidget)
+    avatar = ImageField(label=_('Avatar image'), required=False)
+    branches = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
 
-        if viewerpk != ownerpk:
-            return error403(self.request)
-        else:
-            return ret
-
-    def get(self, request, **kwargs):
-        ret = super(UserEdit, self).get(request, **kwargs)
-        ownerpk = int(self.kwargs.get('pk'))
-        viewer = get_object_or_404(User, pk=self.request.user.pk)
-        viewerpk = viewer.pk
-
-        if viewerpk != ownerpk:
-            return error403(self.request)
-        else:
-            return ret
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.save()
-        return HttpResponseRedirect(reverse('c4c:user_detail', args=(self.object.pk,)))
-
-class C4CUserEdit(generic.edit.UpdateView):
-    template_name = 'c4cuser_edit.html'
-    model = C4CUser
-    fields = ['address','birthday']
-
-    def post(self, request, **kwargs):
-        ret = super(C4CUserEdit, self).post(request, **kwargs)
-        ownerpk = int(self.kwargs.get('pk'))
-        viewer = get_object_or_404(User, pk=self.request.user.pk)
-        viewerpk = viewer.pk
-
-        if viewerpk != ownerpk:
-            return error403(self.request)
-        else:
-            return ret
-
-    def get(self, request, **kwargs):
-        ret = super(C4CUserEdit, self).get(request, **kwargs)
-        ownerpk = int(self.kwargs.get('pk'))
-        viewer = get_object_or_404(User, pk=self.request.user.pk)
-        viewerpk = viewer.pk
-
-        if viewerpk != ownerpk:
-            return error403(self.request)
-        else:
-            return ret
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.save()
-        return HttpResponseRedirect(reverse('c4c:user_detail', args=(self.object.pk,)))
-
-class PasswordForm(forms.Form):
-    pass1 = forms.CharField(label=_('Password'), max_length=10,widget=forms.PasswordInput)
-    pass2 = forms.CharField(label=_('Confirm Password'), max_length=10,widget=forms.PasswordInput)
+    def __init__(self, *args, **kw):
+        super(UserForm, self).__init__(*args, **kw)
+        self.fields['branches'] = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[(a.pk, a.name) for a in C4CBranch.objects.all()])
 
     def clean(self):
-        cleaned_data = super(PasswordForm,self).clean()
-        password = cleaned_data.get('pass1')
-        cpassword = cleaned_data.get('pass2')
+        """ Verify birthday and password"""
+        cleaned_data = super(UserForm, self).clean()
+        if "birthday" in cleaned_data and cleaned_data["birthday"] > datetime.date(datetime.date.today().year - 16, datetime.date.today().month, datetime.date.today().day):
+            raise forms.ValidationError(_("You must be older than 16 years old!"))
+        if "password" in cleaned_data and "password_confirm" in cleaned_data and cleaned_data["password"] != cleaned_data["password_confirm"]:
+            raise forms.ValidationError(_("Passwords do not match"))
+        return cleaned_data
 
-        if password != cpassword:
-            raise forms.ValidationError(_('The two typed passwords are different.'))
 
-def chPassword(request,pk):
-    template_name = 'change_password.html'
-
-    # check access permission
-    if int(pk) != request.user.pk:
-        return error403(request)
-
+def UserEdit(request):
+    template_name = 'base_user_edit.html'
+    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        form = PasswordForm(request.POST)
+        # create a form instance and populate it with data from the request:
+        form = UserForm(request.POST, request.FILES)
+        # check whether it's valid:
         if form.is_valid():
+            request.user.c4cuser.address = form.cleaned_data['address']
+            request.user.c4cuser.birthday = form.cleaned_data['birthday']
+            if form.cleaned_data['avatar'] is not None and form.cleaned_data['avatar']:
+                request.user.c4cuser.avatar = form.cleaned_data['avatar']
+            for branch in C4CBranch.objects.all():
+                branch.group.user_set.remove(request.user)
+            for branch in form.cleaned_data['branches']:
+                request.user.groups.add(C4CBranch.objects.get(pk=branch).group)
+            request.user.c4cuser.save()
+            if form.cleaned_data['password'] is not None and form.cleaned_data['password'] != "":
+                request.user.set_password(form.cleaned_data['password'])
 
-            username = str(request.user.username)
-            formpass = form.cleaned_data['pass1']
-            request.user.set_password(formpass)
-            request.user.save()
+            return HttpResponseRedirect(reverse('c4c:user_detail', args=(request.user.pk,)))
 
-            user = authenticate(username=username,password=formpass)
-            login(request,user)
-            return HttpResponseRedirect(reverse('c4c:user_detail', args=(user.pk,)))
+    # if a GET (or any other method) we'll create a blank form
     else:
-        form = PasswordForm()
+        print([a.pk for a in request.user.c4cuser.get_branches()])
+        form = UserForm(initial={"address": request.user.c4cuser.address, "birthday": request.user.c4cuser.birthday, "branches": [a.pk for a in request.user.c4cuser.get_branches()]})
 
     return render(request, template_name, {'form': form.as_p()})
+
 
 class ResetPassForm(forms.Form):
     email = EmailField(label=_('Email'), max_length=50)
 
     def clean(self):
-        cleaned_data = super(ResetPassForm,self).clean()
+        cleaned_data = super(ResetPassForm, self).clean()
         mailaddr = cleaned_data.get('email')
 
         try:
             user = User.objects.get(email=mailaddr)
         except ObjectDoesNotExist:
             raise forms.ValidationError(_('There is no account with this email address.'))
+
 
 def resetpassword(request):
     template_name = 'resetpassword.html'
@@ -155,7 +111,6 @@ def resetpassword(request):
 
     if request.method == 'POST':
         form = ResetPassForm(request.POST)
-
 
         if form.is_valid():
             email = form.cleaned_data['email']
@@ -169,7 +124,7 @@ def resetpassword(request):
 
             htmly = get_template('email_reset_password.html')
 
-            d = Context({'password' : password, 'user_email' : user})
+            d = Context({'password': password, 'user_email': user})
             html_content = htmly.render(d)
             msg = EmailMultiAlternatives(subject, '', from_email, [to])
             msg.attach_alternative(html_content, "text/html")
@@ -181,4 +136,3 @@ def resetpassword(request):
         form = ResetPassForm()
 
     return render(request, template_name, {'form': form})
-
